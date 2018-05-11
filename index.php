dd2bd4ada6a39a2779b25183e76ceba4b378c7ab
jruiz123 <artjohncajegas@gmail.com>
2018-05-11 23:13:20 +0800

    Update to bootstrap for new layout

--- index.php
+++ index.php
@@ -1,181 +1,22 @@
 <!doctype html>
 <html lang="en">
 	<head>
-	    <!--Let browser know website is optimized for mobile-->
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	    
-	    <!-- Fonts -->
-	    <link rel="stylesheet" href="materialize/fonts/material-icon.css">
-	    <!-- CSS -->
-	    <link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css">
-	    <link rel="stylesheet" type="text/css" href="materialize/css/style.css">
+		<title>GTAT</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	   <!-- CSS -->
+	   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
       <!-- Browser Tab Icon -->
-      <link rel="icon" type="text/css" href="materialize/images/bisdak_logo2.svg">
-
-		<title>Golden Travel and Tours</title>
+      <link rel="icon" type="text/css" href="assets/images/gtat_logo_1.png">
 	</head>
 	<body>
-		<div class="navbar-fixed">
-			<nav class="transparent z-depth-0" role="navigation">
-				<div class="nav-wrapper container">
-					<a id="logo-container" href="#" class="brand-logo">
-						<!-- <img class="brand-logo-image" src="assets/images/bisdak_logo2.svg" height="40" alt=""> -->
-						<i class="material-icons">flight</i>
-						Golden Travel and Tours
-					</a>
-					<ul class="right hide-on-med-and-down">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#">Tour Package</a></li>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Contact Us</a></li>
-					</ul>
-					<ul id="nav-mobile" class="sidenav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#">Tour Package</a></li>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Contact Us</a></li>
-					</ul>
-				<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-				</div>
-			</nav>
-		</div>
-		<div class="row">
-			<div class="carousel carousel-slider center">
-				<div class="carousel-fixed-item center">
-					<a class="btn btn-large waves-effect red white-text darken-text-2">Cebu Tours Package</a>
-				</div>
-				<!-- <a class="carousel-item" href="#one!"><img src="materialize/images/Sample_1.jpg"></a>
-				<a class="carousel-item" href="#two!"><img src="materialize/images/Sample_2.jpg"></a>
-				<a class="carousel-item" href="#three!"><img src="materialize/images/Sample_3.jpg"></a>
-				<a class="carousel-item" href="#four!"><img src="materialize/images/Sample_4.jpg"></a> -->
-        <div class="carousel-item red white-text" href="#one!">
-          <h2>First Panel</h2>
-          <p class="white-text">This is your first panel</p>
-          <img src="materialize/images/Sample_1.jpg">
-        </div>
-        <div class="carousel-item yellow white-text" href="#two!">
-          <h2>Second Panel</h2>
-          <p class="white-text">This is your second panel</p>
-        </div>
-        <div class="carousel-item blue white-text" href="#three!">
-          <h2>Third Panel</h2>
-          <p class="white-text">This is your third panel</p>
-        </div>
-        <div class="carousel-item teal white-text" href="#four!">
-          <h2>Fourth Panel</h2>
-          <p class="white-text">This is your fourth panel</p>
-        </div>
-      </div>
-		</div>
-
-		<!-- <section class="video-container">
-			<video src="https://static.videezy.com/system/resources/previews/000/000/211/original/City%20street%20time%20lapse%20[SaveYouTube.com].mp4" loop playsinline></video>
-			<div class="callout">
-				<h1>A background video</h1>
-				<div>
-					<p>Object-Fit is the CSS Background-Size: Cover for Inline Images &amp; Videos.
-					<br>It's not Microsoft friendly.</p>
-					<a class="waves-effect waves-light btn-large red lighten-2" href="https://codepen.io/DylanMacnab/pen/NjVXEe" target="_blank">Source</a>
-				</div>
-			</div>
-			<div class="support">
-				<a href="http://caniuse.com/#search=object-fit">Browser Support</a><br>
-				<a href="https://github.com/jonathantneal/fitie">Need to support IE &amp; Edge?</a>
-			</div>
-		</section> -->
-		<!-- <div class="row">
-			<div class="carousel carousel-slider center">
-				<div id="carousel-1" class="carousel-item white-text" href="#one!">
-					<h2>First Panel</h2>
-					<p class="white-text">This is your first panel</p>
-				</div>
-				<div id="carousel-2" class="carousel-item yellow white-text" href="#two!">
-					<h2>Second Panel</h2>
-					<p class="white-text">This is your second panel</p>
-				</div>
-				<div id="carousel-3" class="carousel-item blue white-text" href="#three!">
-					<h2>Third Panel</h2>
-					<p class="white-text">This is your third panel</p>
-				</div>
-				<div id="carousel-4" class="carousel-item teal white-text" href="#four!">
-					<h2>Fourth Panel</h2>
-					<p class="white-text">This is your fourth panel</p>
-				</div>
-			</div>
-		</div> -->
-
-		<section>
-			<h1>Hello World!!</h1>
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-
-		</section>
-		<footer class="page-footer green">
-			<div class="container">
-				<div class="row">
-					<div class="col l6 s12">
-						<h5 class="white-text">Bisdak Tours and Travel</h5>
-						<p class="grey-text text-lighten-4">We make sure </p>
-					</div>
-					<div class="col l4 offset-l2 s12">
-						<h5 class="white-text">Links</h5>
-						<ul>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-							<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="footer-copyright">
-				<div class="container">
-					&copy; Copyright 2018 &bull; 
-					<a class="grey-text text-lighten-4" href="#!">Bisdak Tours and Travel</a>
-					 &bull; All Rights Reserved
-					<span class="right">
-					<em>Design by </em>
-					<a class="grey-text text-lighten-4" href="#!">AJC Digital&trade;</a>
-					</span>
-				</div>
-			</div>
-		</footer>
-
-	    <!-- JavaScript -->
-	    <script src="materialize/js/jquery-3.3.1.min.js"></script>
-	    <script src="materialize/js/materialize.min.js"></script>
-	    <script src="materialize/js/init.js"></script>
+		<h1>
+			Hello World!
+		</h1>
+	   <!-- JavaScript -->
+		<script src="assets/js/jquery-slim.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
--- materialize/js/init.js
+++ materialize/js/init.js
@@ -3,31 +3,21 @@
 	$(function(){
 		$('.sidenav').sidenav();
 		$('.carousel.carousel-slider').carousel({
-			fullWidth: true,
-			indicators: true,
+			fullWidth: true
 		});
 	}); // end of document ready
-	
-	// for navbar transparent scroll function
-	// $(function () {
-	//   $(document).scroll(function () {
-	// 	  var $nav = $("nav");
-	// 	  $nav.toggleClass('green', $(this).scrollTop() > $nav.height(), false);
-	// 	});
-	// });
 })(jQuery); // end of jQuery name space
-
-$(document).ready(function() {
-	var s = $("nav");
-	var pos = s.position();					   
-	$(window).scroll(function() {
-		var windowpos = $(window).scrollTop();
-		if (windowpos >= pos.top & windowpos <= s.height()) {
-			s.addClass("transparent");
-			s.removeClass("green");	
-		} else {
-			s.addClass("green");
-			s.removeClass("transparent");	
-		}
-	});
-});
+// $(document).ready(function() {
+// 	var s = $("nav");
+// 	var pos = s.position();					   
+// 	$(window).scroll(function() {
+// 		var windowpos = $(window).scrollTop();
+// 		if (windowpos >= pos.top & windowpos <= s.height()) {
+// 			s.addClass("transparent");
+// 			s.removeClass("green");	
+// 		} else {
+// 			s.addClass("green");
+// 			s.removeClass("transparent");	
+// 		}
+// 	});
+// });
