d95fb1c7c7cfea7abe6e49ccf3159a37ed4035a3
jruiz123 <artjohncajegas@gmail.com>
2018-05-11 23:40:22 +0800

    Update files

--- index.php
+++ index.php
@@ -7,10 +7,34 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	   <!-- CSS -->
 	   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	   <link rel="stylesheet" href="assets/css/custom.css">
       <!-- Browser Tab Icon -->
       <link rel="icon" type="text/css" href="assets/images/gtat_logo_1.png">
 	</head>
 	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">
+				<img src="assets/images/gtat_logo_1.png" height="30" alt="">
+				Golden Travels And Tour
+			</a>
+<!-- 			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button> -->
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					 <span class="navbar-toggler-icon"></span>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Action</a>
+						 <a class="dropdown-item" href="#">Another action</a>
+						 <a class="dropdown-item" href="#">Something else here</a>
+					</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
 		<h1>
 			Hello World!
 		</h1>
