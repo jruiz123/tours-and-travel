d0afa49389caa07d769b55ae0b09bdf497b981c6
jruiz123 <artjohncajegas@gmail.com>
2018-05-12 02:47:45 +0800

    Update files
    added new css and js

--- index.php
+++ index.php
@@ -7,19 +7,51 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	   <!-- CSS -->
 	   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	   <link rel="stylesheet" href="assets/css/custom.css">
+	   <link rel="stylesheet" href="assets/css/style.css">
       <!-- Browser Tab Icon -->
       <link rel="icon" type="text/css" href="assets/images/gtat_logo_1.png">
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<header>
+			<div class="collapse bg-dark" id="navbarHeader">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-md-7 py-4">
+							<h4 class="text-white">About</h4>
+							<p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+						</div>
+						<div class="col-sm-4 offset-md-1 py-4">
+							<h4 class="text-white">Contact</h4>
+							<ul class="list-unstyled">
+								<li><a href="#" class="text-white">Follow on Twitter</a></li>
+								<li><a href="#" class="text-white">Like on Facebook</a></li>
+								<li><a href="#" class="text-white">Email me</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="navbar navbar-dark bg-dark box-shadow">
+				<div class="container d-flex justify-content-between">
+					<a href="#" class="navbar-brand d-flex align-items-center">
+						<img src="assets/images/gtat_logo_1.png" height="30" alt="">
+						Golden Travels And Tour
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+						<!-- <span class="navbar-toggler-icon"></span> -->
+					</button>
+				</div>
+			</div>
+		</header>
+
+		<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<a class="navbar-brand" href="#">
 				<img src="assets/images/gtat_logo_1.png" height="30" alt="">
 				Golden Travels And Tour
 			</a>
-<!-- 			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
-			</button> -->
+			</button>
 			<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 				<ul class="navbar-nav">
 					<li class="nav-item dropdown">
@@ -34,13 +66,16 @@
 					</li>
 				</ul>
 			</div>
-		</nav>
+		</nav> -->
 		<h1>
 			Hello World!
 		</h1>
 	   <!-- JavaScript -->
-		<script src="assets/js/jquery-slim.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
 		<script src="assets/js/popper.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/holder.min.js"></script>
+
 	</body>
 </html>
