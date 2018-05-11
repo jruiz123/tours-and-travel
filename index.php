7db34f9585d24d89d9b5a230c0a3ba06bb2debfe
jruiz123 <artjohncajegas@gmail.com>
2018-05-12 04:31:39 +0800

    added carousel

--- index.php
+++ index.php
@@ -8,12 +8,24 @@
 	   <!-- CSS -->
 	   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	   <link rel="stylesheet" href="assets/css/style.css">
+	   <link rel="stylesheet" href="assets/css/colors.css">
+	   <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.css">
       <!-- Browser Tab Icon -->
       <link rel="icon" type="text/css" href="assets/images/gtat_logo_1.png">
 	</head>
 	<body>
-		<header>
-			<div class="collapse bg-dark" id="navbarHeader">
+		<!-- <nav class="fixed-top transparent">
+			<div class="navbar navbar-dark box-shadow">
+				<div class="container d-flex justify-content-between">
+					<a href="#" class="navbar-brand d-flex align-items-center">
+						<img src="assets/images/gtat_logo_2.png" height="30" alt="">
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="oi oi-menu" title="menu" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+			<div class="collapse" id="navbarHeader">
 				<div class="container">
 					<div class="row">
 						<div class="col-sm-8 col-md-7 py-4">
@@ -31,17 +43,59 @@
 					</div>
 				</div>
 			</div>
-			<div class="navbar navbar-dark bg-dark box-shadow">
-				<div class="container d-flex justify-content-between">
-					<a href="#" class="navbar-brand d-flex align-items-center">
-						<img src="assets/images/gtat_logo_2.png" height="30" alt="">
-					</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-						<!-- <span class="navbar-toggler-icon"></span> -->
-					</button>
+		</nav>
+		-->
+
+		<div class="carousel-body">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<!-- Slide One - Set the background image for this slide in the line below -->
+					<div class="carousel-item active" style="background-image: url('assets/images/Sample_1.jpg')">
+						<div class="carousel-caption d-none d-md-block">
+							<h3>First Slide</h3>
+							<p>This is a description for the first slide.</p>
+						</div>
+					</div>
+					<!-- Slide Two - Set the background image for this slide in the line below -->
+					<div class="carousel-item" style="background-image: url('assets/images/Sample_2.jpg')">
+						<div class="carousel-caption d-none d-md-block">
+							<h3>Second Slide</h3>
+							<p>This is a description for the second slide.</p>
+						</div>
+					</div>
+					<!-- Slide Three - Set the background image for this slide in the line below -->
+					<div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+						<div class="carousel-caption d-none d-md-block">
+							<h3>Third Slide</h3>
+							<p>This is a description for the third slide.</p>
+						</div>
+					</div>
 				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
-		</header>
+		</div>
+
+		<!-- Page Content -->
+		<!-- <section class="py-5">
+			<div class="container">
+				<h1>Full Slider by Start Bootstrap</h1>
+				<p>The background images for the slider are set directly in the HTML using inline CSS. The rest of the styles for this template are contained within the
+				 <code>full-slider.css</code>
+				 file.</p>
+			</div>
+		</section> -->
 
 		<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<a class="navbar-brand" href="#">
@@ -66,15 +120,16 @@
 				</ul>
 			</div>
 		</nav> -->
-		<h1>
+		<!-- <h1>
 			Hello World!
-		</h1>
+		</h1> -->
 	   <!-- JavaScript -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
 		<script src="assets/js/popper.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
 		<script src="assets/js/holder.min.js"></script>
+		<script src="assets/js/mdb.min.js"></script>
 
 	</body>
 </html>
